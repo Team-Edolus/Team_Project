3765db9fd2ed9cb20e6717bc6120788a0faf08ac
kracheff <kracheff@mail.bg>
2016-06-16 01:30:25 +0300

    Creating Credits and Options Forms

---
+++ RPG_AdvancedCS_May/FormCredits.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace RPG_AdvancedCS_May
+{
+    partial class FormCredits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GBOX_Controls = new System.Windows.Forms.GroupBox();
+            this.LBL_MoveUp = new System.Windows.Forms.Label();
+            this.LBL_MoveRight = new System.Windows.Forms.Label();
+            this.LBL_MoveDown = new System.Windows.Forms.Label();
+            this.LBL_MoveLeft = new System.Windows.Forms.Label();
+            this.LBL_DevelopedBy = new System.Windows.Forms.Label();
+            this.LBL_Developer1 = new System.Windows.Forms.Label();
+            this.LBL_Developer2 = new System.Windows.Forms.Label();
+            this.LBL_Developer3 = new System.Windows.Forms.Label();
+            this.LBL_Developer4 = new System.Windows.Forms.Label();
+            this.LBL_Developer5 = new System.Windows.Forms.Label();
+            this.LBL_Developer10 = new System.Windows.Forms.Label();
+            this.LBL_Developer9 = new System.Windows.Forms.Label();
+            this.LBL_Developer8 = new System.Windows.Forms.Label();
+            this.LBL_Developer7 = new System.Windows.Forms.Label();
+            this.LBL_Developer6 = new System.Windows.Forms.Label();
+            this.LBL_Developer11 = new System.Windows.Forms.Label();
+            this.GBOX_Controls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBOX_Controls
+            // 
+            this.GBOX_Controls.Controls.Add(this.LBL_MoveLeft);
+            this.GBOX_Controls.Controls.Add(this.LBL_MoveDown);
+            this.GBOX_Controls.Controls.Add(this.LBL_MoveRight);
+            this.GBOX_Controls.Controls.Add(this.LBL_MoveUp);
+            this.GBOX_Controls.Location = new System.Drawing.Point(12, 12);
+            this.GBOX_Controls.Name = "GBOX_Controls";
+            this.GBOX_Controls.Size = new System.Drawing.Size(527, 105);
+            this.GBOX_Controls.TabIndex = 0;
+            this.GBOX_Controls.TabStop = false;
+            this.GBOX_Controls.Text = "Controls";
+            // 
+            // LBL_MoveUp
+            // 
+            this.LBL_MoveUp.AutoSize = true;
+            this.LBL_MoveUp.Location = new System.Drawing.Point(19, 32);
+            this.LBL_MoveUp.Name = "LBL_MoveUp";
+            this.LBL_MoveUp.Size = new System.Drawing.Size(83, 17);
+            this.LBL_MoveUp.TabIndex = 0;
+            this.LBL_MoveUp.Text = "Move up: W";
+            // 
+            // LBL_MoveRight
+            // 
+            this.LBL_MoveRight.AutoSize = true;
+            this.LBL_MoveRight.Location = new System.Drawing.Point(156, 32);
+            this.LBL_MoveRight.Name = "LBL_MoveRight";
+            this.LBL_MoveRight.Size = new System.Drawing.Size(92, 17);
+            this.LBL_MoveRight.TabIndex = 1;
+            this.LBL_MoveRight.Text = "Move right: D";
+            // 
+            // LBL_MoveDown
+            // 
+            this.LBL_MoveDown.AutoSize = true;
+            this.LBL_MoveDown.Location = new System.Drawing.Point(19, 70);
+            this.LBL_MoveDown.Name = "LBL_MoveDown";
+            this.LBL_MoveDown.Size = new System.Drawing.Size(96, 17);
+            this.LBL_MoveDown.TabIndex = 2;
+            this.LBL_MoveDown.Text = "Move down: S";
+            // 
+            // LBL_MoveLeft
+            // 
+            this.LBL_MoveLeft.AutoSize = true;
+            this.LBL_MoveLeft.Location = new System.Drawing.Point(156, 70);
+            this.LBL_MoveLeft.Name = "LBL_MoveLeft";
+            this.LBL_MoveLeft.Size = new System.Drawing.Size(82, 17);
+            this.LBL_MoveLeft.TabIndex = 3;
+            this.LBL_MoveLeft.Text = "Move left: A";
+            // 
+            // LBL_DevelopedBy
+            // 
+            this.LBL_DevelopedBy.Font = new System.Drawing.Font("Old English Text MT", 16F, System.Drawing.FontStyle.Bold);
+            this.LBL_DevelopedBy.Location = new System.Drawing.Point(6, 137);
+            this.LBL_DevelopedBy.Name = "LBL_DevelopedBy";
+            this.LBL_DevelopedBy.Size = new System.Drawing.Size(533, 31);
+            this.LBL_DevelopedBy.TabIndex = 1;
+            this.LBL_DevelopedBy.Text = "Developed by Team Edolus";
+            this.LBL_DevelopedBy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LBL_Developer1
+            // 
+            this.LBL_Developer1.AutoSize = true;
+            this.LBL_Developer1.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer1.Location = new System.Drawing.Point(217, 329);
+            this.LBL_Developer1.Name = "LBL_Developer1";
+            this.LBL_Developer1.Size = new System.Drawing.Size(92, 28);
+            this.LBL_Developer1.TabIndex = 2;
+            this.LBL_Developer1.Text = "mp3_ico";
+            // 
+            // LBL_Developer2
+            // 
+            this.LBL_Developer2.AutoSize = true;
+            this.LBL_Developer2.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer2.Location = new System.Drawing.Point(27, 269);
+            this.LBL_Developer2.Name = "LBL_Developer2";
+            this.LBL_Developer2.Size = new System.Drawing.Size(112, 28);
+            this.LBL_Developer2.TabIndex = 3;
+            this.LBL_Developer2.Text = "pacaldinho";
+            // 
+            // LBL_Developer3
+            // 
+            this.LBL_Developer3.AutoSize = true;
+            this.LBL_Developer3.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer3.Location = new System.Drawing.Point(27, 209);
+            this.LBL_Developer3.Name = "LBL_Developer3";
+            this.LBL_Developer3.Size = new System.Drawing.Size(109, 28);
+            this.LBL_Developer3.TabIndex = 4;
+            this.LBL_Developer3.Text = "bobsan_id";
+            // 
+            // LBL_Developer4
+            // 
+            this.LBL_Developer4.AutoSize = true;
+            this.LBL_Developer4.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer4.Location = new System.Drawing.Point(27, 389);
+            this.LBL_Developer4.Name = "LBL_Developer4";
+            this.LBL_Developer4.Size = new System.Drawing.Size(93, 28);
+            this.LBL_Developer4.TabIndex = 5;
+            this.LBL_Developer4.Text = "nicecveti";
+            // 
+            // LBL_Developer5
+            // 
+            this.LBL_Developer5.AutoSize = true;
+            this.LBL_Developer5.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer5.Location = new System.Drawing.Point(27, 449);
+            this.LBL_Developer5.Name = "LBL_Developer5";
+            this.LBL_Developer5.Size = new System.Drawing.Size(110, 28);
+            this.LBL_Developer5.TabIndex = 6;
+            this.LBL_Developer5.Text = "mirmeleon";
+            // 
+            // LBL_Developer10
+            // 
+            this.LBL_Developer10.AutoSize = true;
+            this.LBL_Developer10.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer10.Location = new System.Drawing.Point(353, 449);
+            this.LBL_Developer10.Name = "LBL_Developer10";
+            this.LBL_Developer10.Size = new System.Drawing.Size(193, 28);
+            this.LBL_Developer10.TabIndex = 11;
+            this.LBL_Developer10.Text = "blagovest_kracheff";
+            this.LBL_Developer10.Click += new System.EventHandler(this.LBL_Developer10_Click);
+            // 
+            // LBL_Developer9
+            // 
+            this.LBL_Developer9.AutoSize = true;
+            this.LBL_Developer9.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer9.Location = new System.Drawing.Point(353, 389);
+            this.LBL_Developer9.Name = "LBL_Developer9";
+            this.LBL_Developer9.Size = new System.Drawing.Size(94, 28);
+            this.LBL_Developer9.TabIndex = 10;
+            this.LBL_Developer9.Text = "viksataa";
+            // 
+            // LBL_Developer8
+            // 
+            this.LBL_Developer8.AutoSize = true;
+            this.LBL_Developer8.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer8.Location = new System.Drawing.Point(353, 329);
+            this.LBL_Developer8.Name = "LBL_Developer8";
+            this.LBL_Developer8.Size = new System.Drawing.Size(141, 28);
+            this.LBL_Developer8.TabIndex = 9;
+            this.LBL_Developer8.Text = "predator_vs_";
+            // 
+            // LBL_Developer7
+            // 
+            this.LBL_Developer7.AutoSize = true;
+            this.LBL_Developer7.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer7.Location = new System.Drawing.Point(353, 269);
+            this.LBL_Developer7.Name = "LBL_Developer7";
+            this.LBL_Developer7.Size = new System.Drawing.Size(148, 28);
+            this.LBL_Developer7.TabIndex = 8;
+            this.LBL_Developer7.Text = "valiokanibalio";
+            this.LBL_Developer7.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // LBL_Developer6
+            // 
+            this.LBL_Developer6.AutoSize = true;
+            this.LBL_Developer6.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer6.Location = new System.Drawing.Point(353, 209);
+            this.LBL_Developer6.Name = "LBL_Developer6";
+            this.LBL_Developer6.Size = new System.Drawing.Size(117, 28);
+            this.LBL_Developer6.TabIndex = 7;
+            this.LBL_Developer6.Text = "callofduty5";
+            // 
+            // LBL_Developer11
+            // 
+            this.LBL_Developer11.AutoSize = true;
+            this.LBL_Developer11.Font = new System.Drawing.Font("Old English Text MT", 14F, System.Drawing.FontStyle.Bold);
+            this.LBL_Developer11.Location = new System.Drawing.Point(28, 329);
+            this.LBL_Developer11.Name = "LBL_Developer11";
+            this.LBL_Developer11.Size = new System.Drawing.Size(134, 28);
+            this.LBL_Developer11.TabIndex = 12;
+            this.LBL_Developer11.Text = "m.v.manchev";
+            // 
+            // FormCredits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 518);
+            this.Controls.Add(this.LBL_Developer11);
+            this.Controls.Add(this.LBL_Developer10);
+            this.Controls.Add(this.LBL_Developer9);
+            this.Controls.Add(this.LBL_Developer8);
+            this.Controls.Add(this.LBL_Developer7);
+            this.Controls.Add(this.LBL_Developer6);
+            this.Controls.Add(this.LBL_Developer5);
+            this.Controls.Add(this.LBL_Developer4);
+            this.Controls.Add(this.LBL_Developer3);
+            this.Controls.Add(this.LBL_Developer2);
+            this.Controls.Add(this.LBL_Developer1);
+            this.Controls.Add(this.LBL_DevelopedBy);
+            this.Controls.Add(this.GBOX_Controls);
+            this.Name = "FormCredits";
+            this.Text = "Credits";
+            this.GBOX_Controls.ResumeLayout(false);
+            this.GBOX_Controls.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GBOX_Controls;
+        private System.Windows.Forms.Label LBL_MoveLeft;
+        private System.Windows.Forms.Label LBL_MoveDown;
+        private System.Windows.Forms.Label LBL_MoveRight;
+        private System.Windows.Forms.Label LBL_MoveUp;
+        private System.Windows.Forms.Label LBL_DevelopedBy;
+        private System.Windows.Forms.Label LBL_Developer1;
+        private System.Windows.Forms.Label LBL_Developer2;
+        private System.Windows.Forms.Label LBL_Developer3;
+        private System.Windows.Forms.Label LBL_Developer4;
+        private System.Windows.Forms.Label LBL_Developer5;
+        private System.Windows.Forms.Label LBL_Developer10;
+        private System.Windows.Forms.Label LBL_Developer9;
+        private System.Windows.Forms.Label LBL_Developer8;
+        private System.Windows.Forms.Label LBL_Developer7;
+        private System.Windows.Forms.Label LBL_Developer6;
+        private System.Windows.Forms.Label LBL_Developer11;
+    }
+}
---
+++ RPG_AdvancedCS_May/FormOptions.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RPG_AdvancedCS_May
+{
+    partial class FormOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CLBOX_Options = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // CLBOX_Options
+            // 
+            this.CLBOX_Options.FormattingEnabled = true;
+            this.CLBOX_Options.Items.AddRange(new object[] {
+            "Game Music",
+            "Sound Effects",
+            "Others",
+            "Not set yet"});
+            this.CLBOX_Options.Location = new System.Drawing.Point(12, 12);
+            this.CLBOX_Options.Name = "CLBOX_Options";
+            this.CLBOX_Options.Size = new System.Drawing.Size(258, 225);
+            this.CLBOX_Options.TabIndex = 0;
+            this.CLBOX_Options.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // FormOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.CLBOX_Options);
+            this.Name = "FormOptions";
+            this.Text = "FormOptions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox CLBOX_Options;
+    }
+}
