100544bbd3ffa7f5d068f80378f2cec41479d1f7
mirmeleon <morgensterndenise@gmail.com>
2016-10-26 20:07:35 +0200

    MVC initial commit

---
+++ EdolusApp/EdolusApp/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdolusApp.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ EdolusApp/EdolusApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EdolusApp.MvcApplication" Language="C#" %>
